156ac4db93d4fbee5168b9dd737f0cc8b39002f3
JasonDebug <38758441+JasonDebug@users.noreply.github.com>
2019-06-06 20:43:56 -0400

    Initial commit

---
+++ CharlesProxyImporter/CharlesProxyImporter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Fiddler;
+using Newtonsoft.Json;
+using CharlesProxy;
+
+[ProfferFormat("Charles Proxy", "Session List in Charles JSON Format. This is the general format for mobile devices using Charles on mobile devices.", ".chlsj")]
+public class CharlesProxyImporter : ISessionImporter
+{
+    public void Dispose() { /*no-op*/ }
+
+    public Session[] ImportSessions(string sImportFormat, Dictionary<string, object> dictOptions, EventHandler<ProgressCallbackEventArgs> evtProgressNotifications)
+    {
+        string sFilename = string.Empty;
+
+        // Check for command-line, FiddlerScript import, etc
+        if (null != dictOptions)
+        {
+            if (dictOptions.ContainsKey("Filename"))
+            {
+                sFilename = dictOptions["Filename"] as string;
+            }
+        }
+
+        if (string.IsNullOrEmpty(sFilename))
+        {
+            // Example: "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            sFilename = Fiddler.Utilities.ObtainOpenFilename("Open Charles JSON Session File", "Charles JSON Session File (*.chlsj)|*.chlsj");
+        }
+
+        if (string.IsNullOrEmpty(sFilename))
+        {
+            Log("[CharlesProxyTranscoder] User cancelled import.");
+            return null;
+        }
+
+        try
+        {
+            var jsonContent = System.IO.File.ReadAllText(sFilename);
+            CharlesSession[] charlesSessions = CharlesProxy.CharlesSession.FromJson(jsonContent);
+
+            var sessions = new List<Session>();
+
+            for (int i = 0; i < charlesSessions.Length; i++)
+            {
+                var session = charlesSessions[i];
+
+                if (session.Tunnel || session.Status != SessionStatus.COMPLETE)
+                {
+                    // TODO: Likely SSL, display this properly
+                    // I'll need to rebuild the SSL display in Fiddler based on other metadata
+                    // Charles just gives the encrypted bytes in the response body
+                    continue;
+                }
+
+                var reqBytes = ExtractData(session.Request);
+                var respBytes = ExtractData(session.Response);                
+
+                var newSession = new Session(reqBytes, respBytes);
+
+                if (newSession.ResponseHeaders.ExistsAndContains("Transfer-Encoding", "chunked"))
+                {
+                    newSession.ResponseHeaders.Remove("Transfer-Encoding");
+                    newSession.ResponseHeaders.Add("Transfer-Encoding-Removed", "CharlesProxyImporter removed this header as .chlsj are already de-chunked.");
+                }
+
+                sessions.Add(newSession);
+
+                // Notify the caller of our progress
+                if (null != evtProgressNotifications)
+                {
+                    ProgressCallbackEventArgs PCEA =
+                    new ProgressCallbackEventArgs(((i + 1) / (float)charlesSessions.Length),
+                    "wrote " + (i + 1).ToString() + " records.");
+                    evtProgressNotifications(null, PCEA);
+
+                    // If the caller tells us to cancel, abort quickly
+                    if (PCEA.Cancel) { Log("[CharlesProxyTranscoder] Import aborted."); return null; }
+                }
+            }
+
+            return sessions.ToArray();
+        }
+        catch (Exception ex)
+        {
+            FiddlerApplication.Log.LogString($"[CharlesProxyTranscoder] Failed to import Charles JSON Session File: {ex.Message}");
+            return null;
+        }
+    }
+
+    private byte[] ExtractData(HttpSession session)
+    {
+        byte[] headerBytes = new byte[0];
+        byte[] bodyBytes = new byte[0];
+
+        // Header
+        if (session.SizeInfo.headers > 0)
+        {
+            if (null == session.Header)
+            {
+                headerBytes = Encoding.UTF8.GetBytes($"Session request is corrupt.  Headers are null but expected {session.SizeInfo.headers} bytes.");
+            }
+            else
+            {
+                headerBytes = Encoding.UTF8.GetBytes(session.Header.ToString() + Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        // Body
+        if (session.SizeInfo.body > 0)
+        {
+            if (session.Body != null)
+            {
+                if (session.Body.Text != null)
+                {
+                    bodyBytes = Encoding.UTF8.GetBytes(session.Body.Text);
+                }
+                else if (session.Body.Encoded != null && session.Body.Encoding != null && session.Body.Encoding == "base64")
+                {
+                    bodyBytes = Convert.FromBase64String(session.Body.Encoded);
+                }
+            }
+            else
+            {
+                bodyBytes = Encoding.UTF8.GetBytes($"Session request is corrupt.  Headers are null but expected {session.SizeInfo.body} bytes.");
+            }
+        }
+
+        byte[] sessionBytes = new byte[headerBytes.Length + bodyBytes.Length];
+        headerBytes.CopyTo(sessionBytes, 0);
+        bodyBytes.CopyTo(sessionBytes, headerBytes.Length);
+
+        return sessionBytes;
+    }
+
+    private void Log(string text)
+    {
+        FiddlerApplication.Log.LogString(text);
+    }
+}
---
+++ CharlesProxyImporter/CharlesSession.cs
@@ -0,0 +1,110 @@
+﻿namespace CharlesProxy
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Text;
+
+    public class CharlesSession
+    {
+        [JsonProperty("request")]
+        public HttpSession Request { get; set; }
+
+        [JsonProperty("response")]
+        public HttpSession Response { get; set; }
+
+        [JsonProperty("tunnel")]
+        public bool Tunnel { get; set; }
+
+        [JsonProperty("status")]
+        public SessionStatus? Status { get; set; }
+
+        public static CharlesSession[] FromJson(string json) => JsonConvert.DeserializeObject<CharlesSession[]>(json);
+    }
+
+    public class HttpSession
+    {
+        [JsonProperty("sizes")]
+        public SizeInfo SizeInfo { get; set; }
+
+        [JsonProperty("header")]
+        public Header Header { get; set; }
+
+        [JsonProperty("body")]
+        public Body Body { get; set; }
+
+        [JsonProperty("contentEncoding")]
+        public ContentEncoding? ContentEncoding { get; set; }
+    }
+
+    public class SizeInfo
+    {
+        [JsonProperty("body")]
+        public int body { get; set; }
+
+        [JsonProperty("headers")]
+        public int headers { get; set; }
+    }
+
+    public class Header
+    {
+        [JsonProperty("firstLine")]
+        public string FirstLine { get; set; }
+
+        [JsonProperty("headers")]
+        public HeaderElement[] Headers { get; set; }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(FirstLine) && Headers.Length == 0)
+            {
+                return null;
+            }
+
+            return $"{FirstLine}\r\n{string.Join<HeaderElement>("\r\n", Headers)}";
+        }
+    }
+
+    public class HeaderElement
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Value}";
+        }
+    }
+
+    public class Body
+    {
+        [JsonProperty("encoding", NullValueHandling = NullValueHandling.Ignore)]
+        public string Encoding { get; set; }
+
+        [JsonProperty("encoded", NullValueHandling = NullValueHandling.Ignore)]
+        public string Encoded { get; set; }
+
+        [JsonProperty("text", NullValueHandling = NullValueHandling.Ignore)]
+        public string Text { get; set; }
+    }
+
+    public enum ContentEncoding
+    {
+        gzip,
+        brotli
+    }
+
+    public enum SessionStatus
+    {
+        UNKNOWN,
+        SUCCESS,
+        COMPLETE,
+        EXCEPTION,
+        RECEIVING_RESPONSE_BODY,
+        RECEIVING_REQUEST_BODY,
+        RECEIVED_REQUEST_BODY,
+        RECEIVED_RESPONSE_BODY
+    }
+}
